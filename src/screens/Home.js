7646f5f7199bf58377b866a2eb291dbffcc99fed
nicknish09 <nnmarques97@gmail.com>
2019-07-25 18:00:34 -0300

    pequenas refatoracoes

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import { Text, View, TextInput } from 'react-native';
+import { Text, View } from 'react-native';
 
 export default class Home extends Component {
 
--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,4 @@
 import axios from "axios";
-
-// export const baseURL = "http://cgrcjr.herokuapp.com/";
-// export const baseURL = "http://10.0.2.2:3000/";
 export const baseURL = "http://192.168.1.131:3000/";
 
 const api = axios.create({
